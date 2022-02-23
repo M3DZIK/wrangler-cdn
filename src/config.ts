c6ced3e66ac51992d8489f7fb6d240c8998c7253
Medzik <nivua1fn@duck.com>
2022-02-23 22:31:45 +0100

    bump workers-types and update headers

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   userAgent:
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
 }
 
 export default config
